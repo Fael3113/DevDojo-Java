75a9710dbb44a9ef6e94c9945a702edde8ace871
Fael3113 <santiagorafaelsantiago@hotmail.com>
2025-09-03 18:14:59 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/HelloDevDojo.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class HelloDevDojo {
+	//Isso é um comentário de uma linha
+	/*
+	Isso é um comentário
+	de múltiplas
+	linhas
+	 */
+	public static void main(String[] args) {
+		System.out.println("KA-ME-HA-ME-HA");
+	}
+}
