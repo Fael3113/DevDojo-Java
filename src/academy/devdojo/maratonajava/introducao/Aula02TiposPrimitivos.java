f395684acd4f1af5514ed8e6385099702a0af20d
Fael3113 <santiagorafaelsantiago@hotmail.com>
2025-09-04 20:13:30 -0300

    Aula02/03/04
    Terminamos de ver a introdução dos operadores e tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,16 +4,28 @@ public class Aula02TiposPrimitivos {
 	public static void main(String[] args) {
 		//int, double, float, char, byte, short, long, boolean
 		int age = 10;
-		long numberBig = 1000000;
-		double salaryDouble = 2000;
-		float salaryFloat = 2500;
-		byte ageByte = -128;
-		short ageShort = 10;
+		long numberBig = 1000000L;
+		double salaryDouble = 2000.0D;
+		float salaryFloat = 2500.0F;
+		byte ageByte = 127;
+		short ageShort = 32000;
 		boolean verdadeiro = true;
 		boolean falso = false;
-		char character = 'M';
+		char character = 65; //Tabela ASCII
+		char character1 = '\u0041'; //Tabela Unicode
+
+		int casting = (int) 10000000000L;
+		/*Forçar valor, pode ocasionar em problema na capacidade binaria da variável
+		Recomenda-se trocar a variável na maioria dos casos*/
+
+		String nome = "Goku";
 
 		System.out.println("A idade é: "+age+" anos");
+		System.out.println(falso);
+		System.out.println(character);
+		System.out.println(character1);
 
+		System.out.println(casting);
+		System.out.println("Oi, eu sou o "+nome);
 	}
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,69 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+	public static void main(String[] args) {
+		//+ - * /
+		int number01 = 10;
+		int number02 = 20;
+		double result01 = number01 / number02;
+		double result02 = number01 / (double) number02;
+		/*Para se fazer uma conta com resultado em número racional
+		Deve-se usar váriaveis de números racionais, e não inteiros*/
+		System.out.println(result01);
+		System.out.println(result02);
+
+		//%
+		int remainder = 21 % 2;
+		System.out.println(remainder);
+
+		//< > <= >= == !=
+		boolean isTenBiggerThanTwenty = 10 > 20;
+		boolean isTenLessThanTwenty = 10 < 20;
+		boolean isTenEqualTwenty = 10 == 20;
+		boolean isTenEqualTen = 10 == 10;
+		boolean isTenDifferentTwenty = 10 != 20;
+		boolean isTenDifferentTen = 10 != 10;
+		//Não comparar valores incomparáveis
+
+		System.out.println("isTenBiggerThan " + isTenBiggerThanTwenty);
+		System.out.println("isTenLessThan " + isTenLessThanTwenty);
+		System.out.println("isTenEqualTwenty " + isTenEqualTwenty);
+		System.out.println("isTenEqualTen " + isTenEqualTen);
+		System.out.println("isTenDifferentTwenty " + isTenDifferentTwenty);
+		System.out.println("isTenDifferentTen " + isTenDifferentTen);
+
+		//AND (&&) OR (||)
+		int age = 29;
+		float salary = 3500F;
+		boolean isWithinLawBiggerThirty = age >= 30 && salary >= 4612;
+		boolean isWithinLawLowerThirty = age < 30 && salary >= 3381;
+		System.out.println("isWithinLawBiggerThirty " + isWithinLawBiggerThirty);
+		System.out.println("isWithinLawLowerThirty " + isWithinLawLowerThirty);
+
+		double valueTotalCheckingAccount = 200;
+		double valueTotalCDBAccount = 10000;
+		float valuePlaystation = 5000F;
+		boolean isPlaystation5Purchasable = valueTotalCheckingAccount > valuePlaystation || valueTotalCDBAccount > valuePlaystation;
+		System.out.println("isPlaystation5Purchasable " + isPlaystation5Purchasable);
+
+		// = += -= *= /= %=
+		double bonus = 1800;
+		//bonus = bonus + 1000;
+		bonus += 1000; //2800
+		bonus -= 1000; //1800
+		bonus *= 2; //3600
+		bonus /= 3; //1200
+		bonus %= 2; //0
+		System.out.println(bonus);
+
+		//++ --
+		int counter = 0;
+		counter += 1;
+		counter++;
+		counter--;
+		++counter;
+		--counter;
+		int counter2 = 0;
+		System.out.println(++counter2); //Posição do incremento interfere na ordem de execução
+	}
+}
