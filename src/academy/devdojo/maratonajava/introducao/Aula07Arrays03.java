4b5e20796718763653bd9c1d1e4d3f4fa78e3c49
Fael3113 <santiagorafaelsantiago@hotmail.com>
2025-09-08 21:16:01 -0300

    Revisado Arrays e Arrays Multidimensionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03 {
+	public static void main(String[] args) {
+		//int[] numbers = new int[3];
+		//int[] numbers = new int[]{1,2,3,4,5};
+		int[] numbers = {1, 2, 3, 4, 5};
+
+//		for (int i = 0; i < numbers.length; i++) {
+//			System.out.println(numbers[i]);
+//		}
+
+		for (int num : numbers) {
+			System.out.println(num);
+		}
+	}
+}
