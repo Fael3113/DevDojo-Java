0a6eaab06bc38183d28c23e894324936b23232f0
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-01-09 15:23:22 -0300

    Conteudo sobre Associação

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/AssociacaoTeste.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Aluno;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Local;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Professor;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Seminario;
+
+public class AssociacaoTeste {
+	public static void main(String[] args) {
+		Local local = new Local("Grove 13");
+
+		Aluno aluno = new Aluno("Luffy",17);
+		Aluno[] alunosParaSeminario = {aluno};
+
+		Professor professor = new Professor("Rayleigh", "Haki");
+
+		Seminario seminario = new Seminario("Rei dos Piratas", local, alunosParaSeminario);
+		Seminario seminario2 = new Seminario("Derrotar os Cavaleiros de Deus", local, alunosParaSeminario);
+
+		Seminario[] seminariosDisponiveis = {seminario,seminario2};
+
+		professor.setSeminarios(seminariosDisponiveis);
+		professor.imprime();
+	}
+}
