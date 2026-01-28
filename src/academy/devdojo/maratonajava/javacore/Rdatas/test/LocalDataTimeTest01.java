43eb1a6a4b9c55ab31c4effe6118021398381f3f
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-01-28 20:28:26 -0300

    Classe Utilitárias - Date, Calendar, Local(Time/Date/DataTime)
    Também vi a formatação de datas e a internacionalização de números, datas e moeda

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDataTimeTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class LocalDataTimeTest01 {
+	public static void main(String[] args) {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		LocalDate date = LocalDate.parse("2022-08-06");
+		LocalTime time = LocalTime.of(9,45,31);
+		System.out.println(localDateTime);
+		System.out.println(date);
+		System.out.println(time);
+		LocalDateTime ldt1 = date.atTime(time);
+		System.out.println(ldt1);
+		LocalDateTime ldt2 = time.atDate(date);
+		System.out.println(ldt2);
+	}
+}
