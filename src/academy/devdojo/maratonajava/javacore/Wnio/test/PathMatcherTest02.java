25588ee52d269dc2dd4eced0f77ccad8faa99f26
Fael3113 <santiagorafaelsantiago@hotmail.com>
2026-02-13 14:49:04 -0300

    Classe New IO terminada
    Vi como zipar arquivos, fazer a leitura dos arquivos (e configurar padrão de padrão de retorno), e a navegação entre diretórios

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathMatcherTest02.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class  FindAllTestJavaOrClass extends SimpleFileVisitor<Path>{
+	private  PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*Test*.{java,class}");
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		if (matcher.matches(file)){
+			System.out.println(file.getFileName());
+		}
+		return  FileVisitResult.CONTINUE;
+	}
+}
+public class PathMatcherTest02 {
+	public static void main(String[] args) {
+		Path root = Paths.get(".");
+		try {
+			Files.walkFileTree(root, new FindAllTestJavaOrClass());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
